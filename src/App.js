b9215a75042ddecb5bedac014b89a7a4eea36cbb
Harsh Pandit <harsh@technokart.in>
2024-08-04 15:34:26 +0530

    added doughnut chart and made the first row responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from "./components/SidebarComponent";
 import CardElements from "./components/CardElements";
-import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import NetProfitElement from "./components/NetProfitElement";
 
 function App() {
   return (
@@ -13,16 +12,7 @@ function App() {
         <h2 className="page-header">Dashboard</h2>
         <div className="row-01">
           <CardElements />
-          <div className="net-profit">
-            <div className="np-text">
-              <h4 className="np-text-head">netprofit</h4>
-              <h1 className="np-text-body">$ 6759.25</h1>
-              <div className={"np-text-stonks"}>
-                <ArrowDropUpIcon />
-                3%
-              </div>
-            </div>
-          </div>
+          <NetProfitElement />
         </div>
       </div>
     </div>
---
+++ src/components/NetProfitElement.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import DoughnutChart from "./DoughnutChart";
+
+function NetProfitElement() {
+    return (
+        <div className="net-profit">
+            <div className="np-text">
+                <h4 className="np-text-head">Net Profit</h4>
+                <h1 className="np-text-body">$ 6759.25</h1>
+                <div className={"np-text-stonks"}>
+                    <ArrowDropUpIcon />
+                    3%
+                </div>
+            </div>
+            <div className="">
+                <DoughnutChart />
+            </div>
+        </div>
+    )
+}
+
+export default NetProfitElement
